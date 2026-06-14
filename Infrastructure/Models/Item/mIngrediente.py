4c8803f8038948b03a6cbf00eff3acc1f7204805
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-14 16:02:52 -0300

    Atualizar o mIngrediente.py

---
+++ Infrastructure/Models/Item/mIngrediente.py
@@ -0,0 +1,16 @@
+from Models.base import Base, Column, Integer, String, AlEnum, EnumPy
+
+class PeriodoAno(str, EnumPy):
+    VERAO = 'Verão'
+    OUTONO = 'Outono'
+    INVERNO = 'Inverno'
+    PRIMAVERA = 'Primavera'
+
+class Ingrediente(Base):
+    id = Column('ID', Integer, primary_key=True, autoincrement=True)
+    nome = Column('Nome', String, nullable=False)
+    periodo = Column('Periodo', AlEnum(PeriodoAno, values_callable=lambda enum: [e.value for e in enum]), nullable=False)
+
+    def __init__(self, nome, periodo):
+        self.nome = nome
+        self.periodo = periodo
