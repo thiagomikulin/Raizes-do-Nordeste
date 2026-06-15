e391e0185daa84155c2970c29ee06b508d8fa3fc
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-14 21:15:55 -0300

    Atualizar o mPromoFilial.py

--- Infrastructure/Models/Conectores/mPromoFilial.py
+++ Infrastructure/Models/Conectores/mPromoFilial.py
@@ -1,8 +1,9 @@
-from Models.base import Base, Column, ForeignKey
+from Models.base import Base, Column, ForeignKey, Bool
 
 class PromoFilial(Base):
     promocao = Column('CampanhaPromo', ForeignKey('campanhaspromos.id')
     filial = Column('Filial', ForeignKey('filiais.id')
+    ativo = Column('Ativo', Bool, default=True)
 
     def __init__(self, promo, filial):
         self.promocao = promo
