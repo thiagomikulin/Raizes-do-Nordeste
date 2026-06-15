12724468f8c2f5eea409c024afe722e6e92d3cc0
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-15 11:28:18 -0300

    Atualizar o movimentos_router.py

--- API/Routes/Empresa/movimentos_router.py
+++ API/Routes/Empresa/movimentos_router.py
@@ -1,1 +1,20 @@
+from Routes.base import *
+
+movimentos_router = APIRouter(prefix='/movimentos', tags=['movimento'])
+
+# Criar entrada
+
+# Visualizar
+
+# Editar
+
+# Confirmar Entrada
+
+# Itens - Consultar
+
+# Itens - Adicionar
+
+# Itens - Editar
+
+# Itens - Excluir
 
