37dc8493ecb5ceac01474d0130755f0838b1f624
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-15 11:19:18 -0300

    Atualizar o filial_router.py
    mapeamento de rotas a criar

--- API/Routes/Empresa/filial_router.py
+++ API/Routes/Empresa/filial_router.py
@@ -3,6 +3,22 @@ from API.Schemas.Empresa.sFilial import *
 
 filial_router = APIRouter(prefix='/filiais', tags=['filial'])
 
+#Criar
 @filial_router.post('/criar')
 async def criar_filial(schema: CriacaoSchema, sessao:Session = Depends(criar_sessao)):
     return True
+
+# Atualizar
+
+# Listar
+
+# Ativar
+
+# Desativar
+
+# Consultar Vendas
+
+# Associar Campanhas
+
+# Desassociar campanhas
+
