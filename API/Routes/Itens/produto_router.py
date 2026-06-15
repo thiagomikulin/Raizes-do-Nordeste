b934796221cee6e59185ff41106e6fe053e54d9c
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-15 13:13:57 -0300

    Atualizar o produto_router.py

--- API/Routes/Itens/produto_router.py
+++ API/Routes/Itens/produto_router.py
@@ -2,6 +2,17 @@ from fastapi import APIRouter
 
 produto_router = APIRouter(prefix='/produto', tags=['produto'])
 
+# Criar
+
+# Listar
 @produto_router.get('/produto')
 async def get_produto():
     return {'produto':'Acarajé'}
+
+# Editar
+
+# Consultar Quantidade
+
+# Desativar
+
+# Ativar
