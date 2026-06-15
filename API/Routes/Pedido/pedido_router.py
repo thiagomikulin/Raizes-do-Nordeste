bf673a7cec7d3796dda801f3cfe6796d4bd60102
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-15 13:11:55 -0300

    Atualizar o pedido_router.py

--- API/Routes/Pedido/pedido_router.py
+++ API/Routes/Pedido/pedido_router.py
@@ -2,11 +2,29 @@ from fastapi import APIRouter
 
 pedido_router = APIRouter(prefix='/pedido', tags=['pedido'])
 
-@pedido_router.get('/novo')
-async def novo_pedido():
+# Criar
+@pedido_router.get('/criae')
+async def criar_pedido():
     """
     Cria um novo pedido a partir dos dados passados
     """
     return {'pedido':1}
+
+# Editar
+
+# AtualizarStatus
+
+# Consultar
+
+# Cancelar
+
+# Itens - Adicionar
+
+# Itens - Remover
+
+# Itens - Editar
+
+# Itens - Consultar
+
 
 
