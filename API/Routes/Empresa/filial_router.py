ac237e1dea086be9070f61dd10d3a5bb50efaae8
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-15 11:04:08 -0300

    Atualizar o filial_router.py

--- API/Routes/Empresa/filial_router.py
+++ API/Routes/Empresa/filial_router.py
@@ -1,2 +1,8 @@
 from API.Routes.base import *
 from API.Schemas.Empresa.sFilial import *
+
+filial_router = APIRouter(prefix='/filiais', tags=['filial'])
+
+@filial_router.post('/criar')
+async def criar_filial(schema: CriacaoSchema, sessao:Session = Depends(criar_sessao)):
+    return True
