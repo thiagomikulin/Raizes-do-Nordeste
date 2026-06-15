8b055cf129a1dd699ede6dee7ceaf7bac2e178ac
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-15 11:22:38 -0300

    Atualizar o campanha_router.py

--- API/Routes/Empresa/campanha_router.py
+++ API/Routes/Empresa/campanha_router.py
@@ -1,1 +1,20 @@
+from Routes.base import *
+
+campanha_router = APIRouter(prefix='/campanhas', tags=['campanha']) #organizar por pasta pai pode ser interessante
+
+
+# Criar
+
+# Consultar
+
+# Editar
+
+# Ativar
+
+# Desativar
+
+# Vincular Filial
+
+# Desvincular Filial
+
 
