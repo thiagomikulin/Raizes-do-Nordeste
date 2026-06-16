e529833577fb9991b6849e5932407dd6f475a72e
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-16 09:54:00 -0300

    Atualizar o reLogs.py

--- Infrastructure/Repositories/Registros/reLogs.py
+++ Infrastructure/Repositories/Registros/reLogs.py
@@ -1,1 +1,9 @@
-from Repositories.base import Session
+from Infrastructure.Repositories.base import Session
+
+from Infrastructure.Models.Registros.mLogs import Logs
+
+def salvar_log_bd(sessao: Session):
+    novo_log = Log()
+    sessao.add(novo_log)
+    sessao.commit()
+    return True
