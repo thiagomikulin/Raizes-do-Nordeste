1ed5b22cbee037ca6fee1e187dd520a16d5d70b4
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-16 10:28:09 -0300

    Atualizar o ingrediente_router.py

--- API/Routes/Itens/ingrediente_router.py
+++ API/Routes/Itens/ingrediente_router.py
@@ -3,6 +3,9 @@ from Routes.base import *
 ingrediente_router = APIRouter(prefix='ingredientes', tags=['ingrediente'])
 
 # Criar
+@ingrediente_router.post('/criar')
+async def criar_ingredientr(schema: CriacaoSchema, session: Session = Depends (criar_sessao), ator = Depends(verificar_token)):
+    pass
 
 # Consultar
 
