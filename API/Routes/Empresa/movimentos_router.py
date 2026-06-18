014cc725912bf68e7ac87078810efd6261706b3b
Thiago Mikulin <thiago.smikulin@gmail.com>
2026-06-18 13:25:09 -0300

    Atualizar o movimentos_router.py

--- API/Routes/Empresa/movimentos_router.py
+++ API/Routes/Empresa/movimentos_router.py
@@ -3,18 +3,51 @@ from API.Routes.base import *
 movimentos_router = APIRouter(prefix='/movimentos', tags=['movimento'])
 
 # Criar entrada
+@movimentos_router.post('/criar')
+async def criar_movimento():
+    pass
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 # Visualizar
+@movimentos_router.get('/')
+async def listar_movimentos():
+    pass
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 # Editar
+@movimentos_router.put('/{id}')
+async def editar_movimento():
+    pass
 
-# Confirmar Entrada
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# Confirmar movimento
+@movimentos_router.patch('/{id}/confirmar')
+async def confirmar_movimento():
+    pass
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 # Itens - Consultar
+@movimentos_router.get('/{id}/itens')
+async def listar_movimento_itens():
+    pass
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 # Itens - Adicionar
+@movimentos_router.post('/{}/itens/adicionar')
+async def adicionar_movimento_item():
+    pass
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 # Itens - Editar
 
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 # Itens - Excluir
+
+
 
